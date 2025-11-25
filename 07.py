6f21fcf3bd7ee756b21a5de48f72644ab5f48b85
reihan703 <reihanyasfinu@gmail.com>
2025-11-25 14:16:16 +0700

    0.7

---
+++ 07.py
@@ -0,0 +1,11 @@
+class BigClass:
+    def a(self): pass
+    def b(self): pass
+    def c(self): pass
+    def d(self): pass
+    def e(self): pass
+    def f(self): pass
+    def g(self): pass
+    def h(self): pass
+
+print("Class has too many methods.")

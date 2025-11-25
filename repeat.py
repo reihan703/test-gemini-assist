5080a62be4fd7ea0200df0f7f5b61b7df37fb6d4
reihan703 <reihanyasfinu@gmail.com>
2025-11-25 11:37:57 +0700

    repeat myself

---
+++ repeat.py
@@ -0,0 +1,24 @@
+# This program intentionally disobeys the rule:
+# * 0.0 Don't repeat yourself
+
+# Instead of using a loop, it repeats the same action many times.
+
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+print("Hello!")
+
+# Repeated computation instead of a function:
+a = 10 + 20
+b = 10 + 20
+c = 10 + 20
+d = 10 + 20
+e = 10 + 20
+
+print(a, b, c, d, e)
